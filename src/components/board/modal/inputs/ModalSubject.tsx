22ae020193f53ef2a1a6d57004abf0e21a7fa61d
nwejin <147528674+nwejin@users.noreply.github.com>
2024-06-13 00:05:57 +0900

    style

--- src/components/board/modal/inputs/ModalSubject.tsx
+++ src/components/board/modal/inputs/ModalSubject.tsx
@@ -1,9 +1,5 @@
 import { ModalStyle } from '../../../../styles/modal';
 
 export default function ModalSubject() {
-  return (
-    <ModalStyle.SubjectBox>
-      <p>zz</p>
-    </ModalStyle.SubjectBox>
-  );
+  return <ModalStyle.SubjectBox></ModalStyle.SubjectBox>;
 }
