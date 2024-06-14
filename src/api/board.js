fb7f7a536d6af512b5dfdccdee755b279e49d9fa
nwejin <147528674+nwejin@users.noreply.github.com>
2024-06-14 16:03:23 +0900

    fix

--- src/api/board.js
+++ src/api/board.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../data/Board.json')); // 경로 확인
+const router = jsonServer.router(path.join(__dirname, '../data/Board.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
